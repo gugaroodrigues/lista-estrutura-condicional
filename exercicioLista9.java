081cdf844cd73172f59d98e66f199aff4601712f
Gustavo Rodrigues de Bastos <gugaaroodrigues@gmail.com>
2018-04-20 08:57:41 -0300

    correção do arquivo

--- exercicioLista9.java
+++ exercicioLista9.java
@@ -6,14 +6,12 @@ public class exercicioLista9{
         int lado2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Segundo numero:"));
         int lado3 = Integer.parseInt(JOptionPane.showInputDialog(null,"Terceiro numero: "));
         int lado4 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ultimo numero: "));
-        if (lado1 != lado2 && lado1 != lado4){
-            JOptionPane.showMessageDialog(null, "Não é um quadrado");        
-        }if (lado1 != lado3){
+                
+        if (lado1 == lado2 && lado1 == lado3 && lado1 == lado4){
+           JOptionPane.showMessageDialog(null, "É um quadrado");
+        }else{
             JOptionPane.showMessageDialog(null, "Não é um quadrado");
-        }if (lado1 == lado2 && lado1 == lado4){
-            JOptionPane.showMessageDialog(null, "É um quadrado");
-        }if (lado1 != lado3){
-            JOptionPane.showMessageDialog(null, "É um quadrado");        
+
         }
 
 
